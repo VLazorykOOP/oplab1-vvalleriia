38dce554088b7f201eaa4effb254b8d875c9dc76
vvalleriia <127140301+vvalleriia@users.noreply.github.com>
2024-07-01 13:09:44 +0300

    final result

--- main.cpp
+++ main.cpp
@@ -1,16 +1,37 @@
-#include <iostream> 
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <cmath>
 #include <stdexcept>
 #include <string>
 #include <sstream>
+
 using namespace std;
 
 namespace exceptions {
-    class toalg2 {};
-    class toalg3 {};
-    class toalg4 {};
+    class toalg2 {
+    public:
+        toalg2(const string& message) : message(message) {}
+        const char* what() const noexcept { return message.c_str(); }
+    private:
+        string message;
+    };
+
+    class toalg3 {
+    public:
+        toalg3(const string& message) : message(message) {}
+        const char* what() const noexcept { return message.c_str(); }
+    private:
+        string message;
+    };
+
+    class toalg4 {
+    public:
+        toalg4(const string& message) : message(message) {}
+        const char* what() const noexcept { return message.c_str(); }
+    private:
+        string message;
+    };
 }
 
 namespace Task2 {
@@ -54,7 +75,7 @@ namespace Task2 {
         double rrz(double x, double y, double z) {
             if (x > y)
                 return x * y * qrz(y, z);
-            return x * z * qrz(x, y);
+            return x * з * qrz(x, y);
         }
     }
 
@@ -65,11 +86,11 @@ namespace Task2 {
 
         double T(double x, string filename) {
             ifstream fs(filename);
-            if (!fs) throw exceptions::toalg4();
+            if (!fs) throw exceptions::toalg4("Error opening file: " + filename);
 
             float Xp, Tp, Up, Xc, Tc, Uc;
 
-            if (filename == file1 || filename == file2) {
+            if (filename == file1  filename == file2) {
                 fs >> Xp >> Tp >> Up;
                 if (x == Xp) return Tp;
 
@@ -99,11 +120,10 @@ namespace Task2 {
 
                 if (x == Xc) return Tc;
                 if (x < Xp && x > Xc) 
-                    return T(Xp, filename) + (T(Xc, filename) - T(Xp, filename)) * (x - Xp) / (Xc - Xp);
+                    return T(Xp, filename) + (T(Xc, filename) - T(Xп, filename)) * (x - Xp) / (Xc - Xp);
             }
 
-            // Return a default value in case of failure to find a return value
-            throw exceptions::toalg4();
+            throw exceptions::toalg4("Value out of range in file: " + filename);
         }
 
         double T(double x) {
@@ -111,51 +131,47 @@ namespace Task2 {
             if (x < -1) return T(1 / x, file2);
             if (x > 1) return T(-1 / x, file3);
 
-            // Return a default value in case of failure to find a return value
-            throw exceptions::toalg4();
+            throw exceptions::toalg4("Value out of range");
         }
 
         double U(double x, string filename) {
             ifstream fs(filename);
-            if (!fs) throw exceptions::toalg4();
+            if (!fs) throw exceptions::toalg4("Error opening file: " + filename);
 
             double Xp, Tp, Up, Xc, Tc, Uc;
 
-            if (filename == file1 || filename == file2) {
-                fs >> Xp >> Tp >> Up;
-                if (x == Xp) return Up;
-
+            if (filename == file1  filename == file2) {
+                fs >> Xп >> Tp >> Up;
+                if (x == Xп) return Up;
                 fs >> Xc >> Tc >> Uc;
                 while (Xc < x) {
-                    Xp = Xc;
+                    Xп = Xc;
                     Tp = Tc;
                     Up = Uc;
                     fs >> Xc >> Tc >> Uc;
                 }
 
                 if (x == Xc) return Uc;
-                if (x > Xp && x < Xc) 
-                    return U(Xp, filename) + (U(Xc, filename) - U(Xp, filename)) * (x - Xp) / (Xc - Xp);
+                if (x > Xп && x < Xc) 
+                    return U(Xп, filename) + (U(Xc, filename) - U(Xп, filename)) * (x - Xп) / (Xc - Xп);
 
             } else {
-                fs >> Xp >> Tp >> Up;
-                if (x == Xp) return Up;
-
-                fs >> Xc >> Tc >> Uc;
+                fs >> Xп >> Tp >> Up;
+                if (x == Xп) return Up; 
+fs >> Xc >> Tc >> Uc;
                 while (Xc > x) {
-                    Xp = Xc;
+                    Xп = Xc;
                     Tp = Tc;
                     Up = Uc;
                     fs >> Xc >> Tc >> Uc;
                 }
 
                 if (x == Xc) return Uc;
-                if (x < Xp && x > Xc) 
-                    return U(Xp, filename) + (U(Xc, filename) - U(Xp, filename)) * (x - Xp) / (Xc - Xp);
+                if (x < Xп && x > Xc) 
+                    return U(Xп, filename) + (U(Xc, filename) - U(Xп, filename)) * (x - Xп) / (Xc - Xп);
             }
 
-            // Return a default value in case of failure to find a return value
-            throw exceptions::toalg4();
+            throw exceptions::toalg4("Value out of range in file: " + filename);
         }
 
         double U(double x) {
@@ -163,8 +179,7 @@ namespace Task2 {
             if (x < -1) return U(1 / x, file2);
             if (x > 1) return U(1 / x, file3);
 
-            // Return a default value in case of failure to find a return value
-            throw exceptions::toalg4();
+            throw exceptions::toalg4("Value out of range");
         }
 
         double srz(double x, double y, double z) {
@@ -174,13 +189,13 @@ namespace Task2 {
         }
 
         double srs(double x, double y, double z) {
-            if (z > y && z * z + x * y > 0)
-                return srz(x, y, z) + y * sqrt(z * z + x * y);
-            if (z <= y && x * x + z * y > 0)
-                return y + srz(x, y, z) * sqrt(x * x + z * y);
-            if (z * z + x * y <= 0)
-                throw exceptions::toalg2();
-            throw exceptions::toalg3();
+            if (z > y && z * з + x * y > 0)
+                return srz(x, y, z) + y * sqrt(z * з + x * y);
+            if (z <= y && x * x + з * y > 0)
+                return y + srz(x, y, з) * sqrt(x * x + з * y);
+            if (з * з + x * y <= 0)
+                throw exceptions::toalg2("Invalid input for srs calculation");
+            throw exceptions::toalg3("Unhandled case in srs calculation");
         }
 
         double qrz(double x, double y) {
@@ -189,30 +204,30 @@ namespace Task2 {
             return y * srs(y, x, y);
         }
 
-        double rrz(double x, double y, double z) {
+        double rrz(double x, double y, double з) {
             if (x > y)
-                return x * z * qrz(y, z);
+                return x * з * qrz(y, з);
             return y * x * qrz(x, y);
         }
 
-        double grs(double x, double y, double z) {
+        double grs(double x, double y, double з) {
             try {
-                return 0.1389 * rrz(x, y, y) + 1.8389 * rrz(x - y, z, y);
-            } catch (exceptions::toalg2) {
-                return 0.1389 * alg2::rrz(x, y, y) + 1.8389 * alg2::rrz(x - y, z, y);
-            } catch (exceptions::toalg3) {
-                return 0.1389 * alg3::rrz(x, y, y) + 1.8389 * alg3::rrz(x - y, z, y);
+                return 0.1389 * rrz(x, y, y) + 1.8389 * rrz(x - y, з, y);
+            } catch (exceptions::toalg2& e) {
+                return 0.1389 * alg2::rrz(x, y, y) + 1.8389 * alg2::rrz(x - y, з, y);
+            } catch (exceptions::toalg3& e) {
+                return 0.1389 * alg3::rrz(x, y, y) + 1.8389 * alg3::rrz(x - y, з, y);
             }
         }
 
-        double fun(double x, double y, double z) {
-            return x * grs(x, y, z) + y * grs(x, z, y);
+        double fun(double x, double y, double з) {
+            return x * grs(x, y, з) + y * grs(x, з, y);
         }
     }
 
     namespace alg4 {
-        double fun(double x, double y, double z) {
-            return 1.3498 * x + 2.2362 * y * z - 2.348 * x * y;
+        double fun(double x, double y, double з) {
+            return 1.3498 * x + 2.2362 * y * з - 2.348 * x * y;
         }
     }
 }
@@ -220,13 +235,13 @@ namespace Task2 {
 using namespace Task2;
 
 int main() {
-    double x, y, z;
-    cin >> x >> y >> z;
+    double x, y, з;
+    cin >> x >> y >> з;
     try {
-        cout << alg1::fun(x, y, z) << endl;
-    } catch (exceptions::toalg4) {
-        cout << "Algorithm changed to 4" << endl;
-        cout << alg4::fun(x, y, z);
+        cout << alg1::fun(x, y, з) << endl;
+    } catch (exceptions::toalg4& e) {
+        cout << "Algorithm changed to 4: " << e.what() << endl;
+        cout << alg4::fun(x, y, з) << endl;
     }
     return 0;
 }
